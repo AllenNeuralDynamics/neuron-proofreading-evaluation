fc34045e2b8880129303a292d3923b5168a02339
anna-grim <anna.grim@alleninstitute.org>
2026-04-13 18:53:54 +0000

    feat: added eval routines

--- src/neuron_proofreading_evaluation/proofread_splits/evaluate.py
+++ src/neuron_proofreading_evaluation/proofread_splits/evaluate.py
@@ -8,27 +8,22 @@ Code for evaluating the split correction pipeline
 
 """
 
+from copy import deepcopy
+from segmentation_skeleton_metrics.skeleton_metrics import (
+    MergeCountMetric,
+    SplitCountMetric,
+)
+from tqdm import tqdm
+
+import numpy as np
+import pandas as pd
+
+from neuron_proofreading_evaluation.proofread_splits import (
+    data_handling as data_util,
+)
+
 
 # --- Precision-Recall Curves ---
-def compute_precision_recall(gt_graphs, fragment_graphs, labels, proposal_type):
-    # Load data
-    csv_path = os.path.join(s3_prefix, "proposal_summary.csv")
-    proposals_df = data_util.load_proposals_df(csv_path, proposal_type)
-    results_df = create_results_df()
-
-    # Compute metrics
-    compute_splits_and_merges(
-        gt_graphs, fragment_graphs, labels, proposals_df, results_df
-    )
-    compute_precision_recall_from_df(results_df)
-
-    # Save results
-    suffix = ("_" + proposal_type) if isinstance(proposal_type, str) else ""
-    path = os.path.join(output_dir, f"metrics_varying_threshold{suffix}.csv")
-    results_df.to_csv(path)
-
-
-
 def compute_precision_recall_from_df(results_df):
     initial_merges = results_df.loc[1.0, "# Merges"]
     initial_splits = results_df.loc[1.0, "# Splits"]
@@ -44,8 +39,6 @@ def compute_precision_recall_from_df(results_df):
         results_df.loc[t, "Recall"] = round(recall, 4)
         results_df.loc[t, "F1"] = round(f1, 4)
 
-    print(results_df)
-
 
 def compute_splits_and_merges(
     gt_graphs, fragment_graphs, labels, proposals_df, results_df
@@ -59,14 +52,29 @@ def compute_splits_and_merges(
             deepcopy(gt_graphs),
             deepcopy(fragment_graphs),
             labels,
-            proposals_df
+            proposals_df,
         )
 
         # Count merges (if applicable)
-        if t > 0.2:
+        if t > 0.19:
             merge_cnts = merge_count_metric(gt_graphs_t, fragment_graphs_t)
             results_df.loc[t, "# Merges"] = merge_cnts["# Merges"].sum()
 
         # Count splits
         split_cnts = split_count_metric(gt_graphs_t)
         results_df.loc[t, "# Splits"] = split_cnts["# Splits"].sum()
+
+
+# --- Helpers ---
+def create_results_df(dt=0.05):
+    columns = [
+        "Threshold",
+        "# Merges",
+        "# Splits",
+        "Precision",
+        "Recall",
+        "F1",
+    ]
+    results_df = pd.DataFrame(columns=columns)
+    results_df["Threshold"] = np.round(np.arange(0, 1 + dt, dt), decimals=2)
+    return results_df.set_index("Threshold")
